68db33bae50ce3c5404b426ca1cf451d3aa0b650
unknown <tienminhhpvn99@gmail.com>
2022-06-05 20:47:34 +0700

    init file

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Happy BirthDay Bé Iu ! 🖐🖐🖐',
+    introDesc: `Anh có điều nay muốn nói, em có muốn nghe hăm`,
+    btnIntro: 'OK',
+    title: 'Anh biết Em yêu Anh nhiều lắm phải không phải không ? 😙',
+    desc: 'Em không trả lời hoặc thoát ra, tức là Em yêu Anh đó nha :> ',
+    btnYes: 'Có, em thật sự rất rất yêu anh 💖💖💖',
+    btnNo: '💔 Anh mơ à ! 💔 ',
+    question:'Nói cho Anh biết đi vì sao Em thích anh 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Tại vì anh đẹp trai và đáng yêu đó Ahiiiii 😊😊',
+    mess: 'Anh biết mà, Anh cũng yêu em lắm 🥰. Love you 300.000 😘😘 and Happy Birthday',
+    messDesc: '💬💬 Quay trở lại box chat của chúng ta nhaa 💬💬',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://m.me/vutien.minh.98'
+}
